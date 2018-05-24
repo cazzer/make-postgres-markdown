89fc3ed1ec9e6f923c302d5d157ac14d6ec2ba69
Caleb Brewer <caleb@cazzer.io>
2018-05-24 09:10:46 -0700

    abstracting the cli away from the business

---
+++ src/index.js
@@ -0,0 +1,88 @@
+
+import Debug from 'debug'
+import fs from 'fs'
+import json2md from 'json2md'
+import pgStructure from 'pg-structure'
+
+const d = new Debug('make-postgres-markdown')
+
+
+export default async function makeMarkdown(options) {
+  console.time('make-postgres-markdown')
+  d('Parsing schema')
+
+  const ignore = options.ignore
+    ? new RegExp(options.ignore)
+    : false
+
+  const db = await pgStructure(options, [options.schema])
+  d('Building JSON representation...')
+  const markdown = []
+  const schema = db.schemas.get('public')
+  const tables = schema.tables
+
+  d('tables')
+  markdown.push({ h1: 'Tables' })
+  for (let [name, table] of tables) {
+    if (ignore && ignore.exec(name)) {
+      continue
+    }
+
+    markdown.push({ h2: name })
+    const markdownTable = {
+      headers: [
+        'column',
+        'comment',
+        'type',
+        'default',
+        'constraints',
+        'values'
+      ],
+      rows: []
+    }
+
+    for (let [name, column] of table.columns) {
+      markdownTable.rows.push([
+        name || '',
+        column.comment || '',
+        column.type || '',
+        column.default || '',
+        renderConstraints(column) || '',
+        column.enumValues ? column.enumValues.join(', ') : ''
+      ])
+    }
+    markdown.push({ table: markdownTable })
+  }
+
+  d('Converting JSON to markdown')
+  const output = json2md(markdown)
+
+  d('Writing output')
+  fs.writeFileSync(options.output, `---
+title: Database Documentation
+
+search: true
+---
+
+${output}
+  `)
+
+  d('Finished')
+  console.timeEnd('make-postgres-markdown')
+}
+
+function renderConstraints(column) {
+  const constraints = []
+
+  if (!column.allowNull) {
+    constraints.push('NOT NULL')
+  }
+
+  for (let [constraintName, constraint] of column.foreignKeyConstraints) {
+    for (let [name, column] of constraint.columns) {
+      constraints.push(`[${name}](#${constraint.referencedTable.name})`)
+    }
+  }
+
+  return constraints.length && constraints.join(', ')
+}
--- src/make-postgres-markdown.js
+++ src/make-postgres-markdown.js
@@ -1,12 +1,7 @@
 #!/usr/bin/env node
-
-import Debug from 'debug'
 import program from 'commander'
-import fs from 'fs'
-import json2md from 'json2md'
-import pgStructure from 'pg-structure'
 
-const d = new Debug('make-postgres-markdown')
+import makeMarkdown from './index'
 
 program
   .version('0.1.0')
@@ -20,82 +15,4 @@ program
   .option('-i, --ignore <ignore>', 'Pattern of objects to ignore')
   .parse(process.argv)
 
-console.time('make-postgres-markdown')
-d('Parsing schema')
-
-const ignore = program.ignore
-  ? new RegExp(program.ignore)
-  : false
-
-pgStructure(program, [program.schema])
-  .then(db => {
-    d('Building JSON representation...')
-    const markdown = []
-    const schema = db.schemas.get('public')
-    const tables = schema.tables
-
-    d('tables')
-    markdown.push({ h1: 'Tables' })
-    for (let [name, table] of tables) {
-      if (ignore && ignore.exec(name)) {
-        continue
-      }
-
-      markdown.push({ h2: name })
-      const markdownTable = {
-        headers: [
-          'column',
-          'comment',
-          'type',
-          'default',
-          'constraints',
-          'values'
-        ],
-        rows: []
-      }
-
-      for (let [name, column] of table.columns) {
-        markdownTable.rows.push([
-          name || '',
-          column.comment || '',
-          column.type || '',
-          column.default || '',
-          renderConstraints(column) || '',
-          column.enumValues ? column.enumValues.join(', ') : ''
-        ])
-      }
-      markdown.push({ table: markdownTable })
-    }
-
-    d('Converting JSON to markdown')
-    const output = json2md(markdown)
-
-    d('Writing output')
-    fs.writeFileSync(program.output, `---
-title: Database Documentation
-
-search: true
----
-
-${output}
-    `)
-
-    d('Finished')
-    console.timeEnd('make-postgres-markdown')
-  })
-
-function renderConstraints(column) {
-  const constraints = []
-
-  if (!column.allowNull) {
-    constraints.push('NOT NULL')
-  }
-
-  for (let [constraintName, constraint] of column.foreignKeyConstraints) {
-    for (let [name, column] of constraint.columns) {
-      constraints.push(`[${name}](#${constraint.referencedTable.name})`)
-    }
-  }
-
-  return constraints.length && constraints.join(', ')
-}
+makeMarkdown(program)
